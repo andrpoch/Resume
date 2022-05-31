a6884478cade65840baacf3cf66d39875f4f7987
andrpoch <andrpoch25@gmail.com>
2022-05-31 18:23:15 +0300

    finish header

--- resume/src/components/App.js
+++ resume/src/components/App.js
@@ -1,5 +1,6 @@
-import './App.css';
+import React from 'react';
 import Navbar from './NavBar/NavBar';
+import './App.css';
 
 export default function App() {
   return (
--- resume/src/components/NavBar/NavBar.js
+++ resume/src/components/NavBar/NavBar.js
@@ -8,47 +8,44 @@ import {
    AiOutlineLinkedin,
 
 } from 'react-icons/ai';
+import { BsListStars } from "react-icons/bs";
 import { IoIosContact } from 'react-icons/io';
 import { FaTelegramPlane } from 'react-icons/fa';
 import s from './NavBar.module.css';
-import Services from '../../assets/images/Services';
 
 export default function NavBar() {
    return (
          <header className={s.header}>
-            <nav className={s.navigation}>
-            <NavLink className={s.navigationLink}
-            activeClassName={s.navigationActive}  to='/'>
-               <p className={s.text}>Home</p>
+            <nav className={s.nav}>
+            <NavLink to='/' className={s.link}
+        >
+               Home
                <AiOutlineHome style={{ width: "25px", height: "25px" }} />
             </NavLink>
-            <NavLink className={s.navigationLink}
-            activeClassName={s.navigationActive}    to='/about'>
-               <p className={s.text}>About</p>
+            <NavLink to='/about' className={s.link}
+              >
+               About
                <AiOutlineInfoCircle style={{ width: "25px", height: "25px" }} />
             </NavLink>
-            <NavLink className={s.navigationLink}
-            activeClassName={s.navigationActive}    to='/services'>
-               <p className={s.text}>Services</p>
-               {/* <GrServices style={{ width: "25px", height: "25px" }} /> */}
-               <Services />
+            <NavLink to='/services' className={s.link}    >
+               Services
+               <BsListStars style={{ width: "25px", height: "25px" }} />
             </NavLink>
-            <NavLink className={s.navigationLink}
-            activeClassName={s.navigationActive}    to='/portfolio'>
-               <p className={s.text}>Portfolio</p>
+            <NavLink to='/portfolio' className={s.link}  >
+               Portfolio
                <AiOutlineFolder style={{ width: "25px", height: "25px" }} />
             </NavLink>
-            <NavLink className={s.navigationLink}
-            activeClassName={s.navigationActive}    to='/contact'>
-               <p className={s.text}>Contact</p>
+            <NavLink to='/contact' className={s.link}
+         >
+              Contact
                <IoIosContact style={{ width: "25px", height: "25px" }} />
                </NavLink>
          </nav>
          <div className={s.socialWrapper}>
             <ul className={s.socialList}>
                <li ><a className={s.socialLink} href="https://github.com/andrpoch"><AiFillGithub style={{ width: "25px", height: "25px" }} /></a></li>
-               <li><a className={s.socialLink} href="https://github.com/andrpoch"><AiOutlineLinkedin style={{ width: "25px", height: "25px" }} /></a></li>
-               <li><a className={s.socialLink} href="https://github.com/andrpoch"><FaTelegramPlane style={{ width: "25px", height: "25px" }} /></a></li>
+               <li><a className={s.socialLink} href="https://www.linkedin.com/in/andr-poch/"><AiOutlineLinkedin style={{ width: "25px", height: "25px" }} /></a></li>
+               <li><a className={s.socialLink} href="https://t.me/andr_poch"><FaTelegramPlane style={{ width: "25px", height: "25px" }} /></a></li>
             </ul>
          </div>
          </header>
