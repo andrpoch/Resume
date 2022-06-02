78d05d0758577b02b3449f73758763d5b79a9f59
andrpoch <andrpoch25@gmail.com>
2022-06-02 12:32:44 +0300

    add

--- resume/src/components/App.js
+++ resume/src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Route,Routes } from 'react-router-dom';
 import Navbar from './NavBar/NavBar';
 import './App.css';
+import HomePage from '../pages/HomePage';
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <main>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </main>
     </>
   )
 };
