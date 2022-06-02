e3e1096fc7154ad48738e7a78b81f34189b81b65
andrpoch <andrpoch25@gmail.com>
2022-06-02 13:53:07 +0300

    work home

--- resume/src/components/Home/Home.jsx
+++ resume/src/components/Home/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import s from './Home.module.css';
-
+import Container from '../Container/Container'
 export default function Home() {
    return (
-      <>Home</>
+         <Container>
+      <section className={s.home}>
+           <div className={s.image}></div>
+         <h1 className={s.name}>Andrew Pochapskyi</h1>
+            <p className={s.title}>I'm a FullStack Developer</p>
+      </section>
+         </Container>
    )
 };
