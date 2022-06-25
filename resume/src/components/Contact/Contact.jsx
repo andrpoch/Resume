190471e3bf572e216ef2fd60a13ee9769dcb036e
andrpoch <andrpoch25@gmail.com>
2022-06-25 08:26:32 +0300

    contact section finish markup

--- resume/src/components/Contact/Contact.jsx
+++ resume/src/components/Contact/Contact.jsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import s from './Contact.module.css';
 import Container from '../Container/Container';
 
-export default function Contact() {
+export default function ContactUs ()  {
+   const form = useRef();
+   const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
+       .then((result) => {
+         alert("Message Sent, We will get back to you shortly", result.text);
+       }, (error) => {
+          alert("An error occurred, Please try again", error.text);
+      });
+  };
+
    return (
-    <Container>
-         <section className={s.contact}>
-             <p className={s.mainText}>Contact</p>
-               <h2 className={s.mainName}>Get in Touch</h2>
-         </section>
-      </Container>
-   )
+   <Container>
+      <section className={s.contact}>
+         <p className={s.mainText}>Contact</p>
+            <h2 className={s.mainName}>Get in Touch</h2>
+            <div className={s.formWrapper}>
+               <p className={s.text}>
+                  Please fill out the form on this section to contact with me. Or call between 9:00 and 20:00, Monday through Friday
+               </p>
+               <form ref={form} onSubmit={sendEmail}>
+                  <div className={s.inputWrapper}>
+                  <label>Name</label>
+                     <input className={s.input} required type="text" name="name" />
+                  </div>
+                  <div className={s.inputWrapper}>
+                  <label>Email</label>
+                     <input className={s.input} required type="email" name="email" />
+                  </div>
+                  <div className={s.inputWrapper}>
+                  <label>Message</label>
+                     <textarea className={s.input} required name="message" />
+                  </div>
+                     <input className={s.input} type="submit" value="Send" />
+               </form>
+            </div>
+            <div className="mapWrapper">
+               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11970.741334879223!2d28.470005161531084!3d49.23310390512744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472d5b65195a6489%3A0xcbd4e159eedd74fe!2z0JLQuNC90L3QuNGG0LAsINCS0LjQvdC90LjRhtC60LDRjyDQvtCx0LvQsNGB0YLRjCwgMjEwMDA!5e0!3m2!1sru!2sua!4v1656131783870!5m2!1sru!2sua" title="Vinnitsa map" width="600" height="450" style={{ border: 0 }}  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+            </div>
+      </section>
+   </Container>
+  );
 };
--- resume/src/components/Portfolio/Portfolio.jsx
+++ resume/src/components/Portfolio/Portfolio.jsx
@@ -18,46 +18,46 @@ export default function Portfolio() {
                   <li className={s.item}>
                      <div className={s.overlay}>
                         <div className={s.btnWrapper}>
-                           <a href='https://wallet-react-app.netlify.app/login' className={s.btn} target='_blank'>Site</a>
-                           <a href='https://github.com/andrpoch/wallet-react-project.git' className={s.btn} target='_blank'>Code</a>
+                           <a href='https://wallet-react-app.netlify.app/login' className={s.btn} target='_blank' rel="noopener noreferrer">Site</a>
+                           <a href='https://github.com/andrpoch/wallet-react-project.git' className={s.btn} target='_blank' rel="noopener noreferrer">Code</a>
                         </div>
-                        <img src={wallet} alt='Wallet image' className={s.img} />
+                        <img src={wallet} alt='Wallet' className={s.img} />
                      </div>
                   </li>
                    <li className={s.item}>
                      <div className={s.overlay}>
                         <div className={s.btnWrapper}>
-                           <a href='https://frabjous-seahorse-e40308.netlify.app/' className={s.btn} target='_blank'>Site</a>
-                           <a href='https://github.com/andrpoch/goit-react-hw-08-phonebook.git' className={s.btn} target='_blank'>Code</a>
+                           <a href='https://frabjous-seahorse-e40308.netlify.app/' className={s.btn} target='_blank' rel="noopener noreferrer">Site</a>
+                           <a href='https://github.com/andrpoch/goit-react-hw-08-phonebook.git' className={s.btn} target='_blank' rel="noopener noreferrer">Code</a>
                         </div>
-                        <img src={contacts} alt='Contacts image' className={s.img} />
+                        <img src={contacts} alt='Contacts' className={s.img} />
                      </div>
                   </li>
                    <li className={s.item}>
                      <div className={s.overlay}>
                         <div className={s.btnWrapper}>
-                           <a href='https://memx86.github.io/team-project-js-tmdb/c' className={s.btn} target='_blank'>Site</a>
-                           <a href='https://github.com/andrpoch/team-project-js-filmoteka.git' className={s.btn} target='_blank'>Code</a>
+                           <a href='https://memx86.github.io/team-project-js-tmdb/c' className={s.btn} target='_blank' rel="noopener noreferrer">Site</a>
+                           <a href='https://github.com/andrpoch/team-project-js-filmoteka.git' className={s.btn} target='_blank' rel="noopener noreferrer">Code</a>
                         </div>
-                        <img src={filmoteka} alt='Filmoteka image' className={s.img} />
+                        <img src={filmoteka} alt='Filmoteka ' className={s.img} />
                      </div>
                   </li>
                    <li className={s.item}>
                      <div className={s.overlay}>
                         <div className={s.btnWrapper}>
-                           <a href='https://vatakusi.github.io/team-2-pro/' className={s.btn} target='_blank'>Site</a>
-                           <a href='https://github.com/andrpoch/html-css-project.git' className={s.btn} target='_blank'>Code</a>
+                           <a href='https://vatakusi.github.io/team-2-pro/' className={s.btn} target='_blank' rel="noopener noreferrer">Site</a>
+                           <a href='https://github.com/andrpoch/html-css-project.git' className={s.btn} target='_blank' rel="noopener noreferrer">Code</a>
                         </div>
-                        <img src={iceCream} alt='IceCream image' className={s.img} />
+                        <img src={iceCream} alt='IceCream ' className={s.img} />
                      </div>
                   </li>
                    <li className={s.item}>
                      <div className={s.overlay}>
                         <div className={s.btnWrapper}>
-                           <a href='https://github.com/andrpoch/slides.git' className={s.btn} target='_blank'>Code</a>
-                           <a href='https://andrpoch.github.io/slides/' className={s.btn} target='_blank'>Site</a>
+                           <a href='https://github.com/andrpoch/slides.git' className={s.btn} target='_blank' rel="noopener noreferrer">Code</a>
+                           <a href='https://andrpoch.github.io/slides/' className={s.btn} target='_blank' rel="noopener noreferrer">Site</a>
                         </div>
-                        <img src={slides} alt='Slides image' className={s.img} />
+                        <img src={slides} alt='Slides ' className={s.img} />
                      </div>
                   </li>
                </ul>
