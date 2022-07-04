76e7683b24ea6736dbad1138e8f258e2190f15a2
andrpoch <andrpoch25@gmail.com>
2022-07-04 14:45:19 +0300

    fix styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,27 +3,15 @@ import s from './Footer.module.scss';
 import { AiFillGithub, AiOutlineLinkedin } from 'react-icons/ai';
 import { FaTelegramPlane } from 'react-icons/fa';
 
-import { motion } from "framer-motion"
-
 export default function Footer() {
    return (
       <footer className={s.footer}>
-         <motion.div
-         initial={{ x: "100%" }}
-animate={{ x: "calc(22vw - 100%)" }}
->
             <p className={s.footer__text}>© Copyright 2022. All rights are reserved.</p>
-            </motion.div>
-                     <motion.div
-         initial={{ x: "100%" }}
-animate={{ x: "calc(13vw - 100%)" }}
->
    <ul className={s.footer__list}>
                <li ><a className={s.footer__link} href="https://github.com/andrpoch"><AiFillGithub style={{ width: "25px", height: "25px" }} /></a></li>
                <li><a className={s.footer__link} href="https://www.linkedin.com/in/andr-poch/"><AiOutlineLinkedin style={{ width: "25px", height: "25px" }} /></a></li>
                <li><a className={s.footer__link} href="https://t.me/andr_poch"><FaTelegramPlane style={{ width: "25px", height: "25px" }} /></a></li>
             </ul>
-            </motion.div>
          </footer>
    )
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,21 +4,21 @@ import Container from '../Container/Container';
 
 export default function Home() {
    return (
-         <Container>
+   <Container>
       <section className={s.home}>
-           <div className={s.image}></div>
-         <h1 className={s.name}>Andrew Pochapskyi</h1> 
-         <div className={s.typewriter}>
-    <p className={s.title}>I'm</p>
- <span className={s.type}>
-  <span>
-    <span> Web Developer</span>
-    <span> Front-End Developer</span>
-    <span> Full Stack developer</span>
-  </span>
-           </span>
+         <div className={s.home__image}></div>
+            <h1 className={s.home__name}>Andrew Pochapskyi</h1>
+         <div className={s.home__typewriter}>
+            <p className={s.home__title}>I'm</p>
+         <span className={s.home__type}>
+            <span>
+            <span> Web Developer</span>
+            <span> Front-End Developer</span>
+            <span> Full Stack developer</span>
+            </span>
+            </span>
          </div>
       </section>
-         </Container>
+   </Container>
    )
 };
