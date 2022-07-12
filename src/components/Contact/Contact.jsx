ce9b4d64c439f7e4a11e32da494cc097def084bc
andrpoch <andrpoch25@gmail.com>
2022-07-12 07:41:31 +0300

    finish mobile markup and all site

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -61,7 +61,7 @@ const sendEmail = (object) => {
             </Form>
             )}
          </Formik>
-               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11970.741334879223!2d28.470005161531084!3d49.23310390512744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472d5b65195a6489%3A0xcbd4e159eedd74fe!2z0JLQuNC90L3QuNGG0LAsINCS0LjQvdC90LjRhtC60LDRjyDQvtCx0LvQsNGB0YLRjCwgMjEwMDA!5e0!3m2!1sru!2sua!4v1656131783870!5m2!1sru!2sua" title="Vinnitsa map" width="600" height="450" style={{ border: 0 }}  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+               <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11970.741334879223!2d28.470005161531084!3d49.23310390512744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472d5b65195a6489%3A0xcbd4e159eedd74fe!2z0JLQuNC90L3QuNGG0LAsINCS0LjQvdC90LjRhtC60LDRjyDQvtCx0LvQsNGB0YLRjCwgMjEwMDA!5e0!3m2!1sru!2sua!4v1656131783870!5m2!1sru!2sua" title="Vinnitsa map" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" className={s.map} />
             </div>
       </section>
    </Container>);
